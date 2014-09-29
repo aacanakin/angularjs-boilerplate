9ca667c45e18cd6705042fae99c359835f255eaa
Aras Can Akin <aacanakin@gmail.com>
2014-09-29 08:57:03 +0300

    remove domReady config path

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,6 @@
 require.config({
 
 	paths: {
-		// 'domReady': '../../bower_components/requirejs-domready/domReady',
 		'angular': '../../bower_components/angular/angular',
 		'angular-route': '../../bower_components/angular-route/angular-route'
 	},
